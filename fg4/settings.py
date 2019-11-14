c423141e66c3073d4f5d2899a9a35dc4e4d638ee
rodrigo <rodrigoddc1@gmail.com>
2019-11-14 16:23:13 -0300

    Configuring the app

--- fg4/settings.py
+++ fg4/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'fg4.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
