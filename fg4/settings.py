0f443216f1718100f9e5ff04e6de3fb23f5b2882
rodrigo <rodrigoddc1@gmail.com>
2019-11-14 16:01:20 -0300

    Configuring the app

--- fg4/settings.py
+++ fg4/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/'),
+    os.path.join(BASE_DIR, 'apps/core/static'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
